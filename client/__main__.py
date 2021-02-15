c5989315196f197f95448356867a0260630c0a83
outstandingboy <outstandingboy@kakao.com>
2021-02-15 22:11:05 +0900

    feat: 이미지 -> 카메라로 변경

--- client/__main__.py
+++ client/__main__.py
@@ -1,8 +1,8 @@
 from .face import camera
 
 def main():
-    camera.startTest()
-    # camera.startCapture()
+    # camera.startTest()
+    camera.startCapture()
 
 if __name__ == "__main__":
     main()
