271d2377e8ebc218b8a518fa325099bb14ba189c
CastleJo <josw631@gmail.com>
2021-02-18 22:31:56 +0900

    refactor(match.py): face detector -> mask detection 순서 변경

--- server/routes/match.py
+++ server/routes/match.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 from flask import request, Blueprint
 from server.tools.face_tool import FaceTool
+import face_recognition
 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
@@ -58,7 +59,36 @@ def match():
     net.setInput(blob)
     detections = net.forward()
 
+    (h, w) = np.shape(frame)[:2]
 
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0,0,i,2]
 
+        if confidence < 0.5: #임계점 이하일시 continue
+            continue
+        
+        #mask detection
+        face_location = detections[0,0,i,3:7] * np.array([w,h,w,h])
+        (left, top, right, bottom) = face_location.astype("int")
+        top = max(0,top)
+        bottom = min(h-1,bottom)
+        left = max(0,left)
+        right = min(w-1,right)        
+
+        print(top,bottom,left,right)
+
+        face = frame[top:bottom,left:right]
+        face = cv2.cvtColor(face,cv2.COLOR_BGR2RGB)
+        face = cv2.resize(face, (224, 224))
+        face = img_to_array(face)
+        face = preprocess_input(face)
+        face = np.expand_dims(face, axis=0)
+
+        (mask,withoutMask) = model.predict(face)[0]
+        if mask>withoutMask:
+            print("Mask")
+        else:
+            print("No Mask")
+        
 
     return {'data': result}, 200
