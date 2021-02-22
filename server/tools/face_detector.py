a2e50ddecd9d7283aae0af86b3b8163acc59fb1f
CastleJo <josw631@gmail.com>
2021-02-22 20:06:38 +0900

    refactor(face_detector.py): face detector 모듈화

--- server/tools/face_detector.py
+++ server/tools/face_detector.py
@@ -1,18 +1,48 @@
 import cv2
+import numpy as np
+import sys
 
-#net = cv2.dnn.readNet('server/face_detector/deploy.prototxt','server/face_detector/res10_300x300_ssd_iter_140000.caffemodel')
+#__net = cv2.dnn.readNet('server/face_detector/deploy.prototxt','server/face_detector/res10_300x300_ssd_iter_140000.caffemodel')
 
-class FaceDetector():
-    def __init__(self, prototxt, model):
-        self._net = cv2.dnn.readNet(prototxt, model)
+class FaceDetector:
+    __net = None
 
+    def __init__(self, prototxtPath, modelPath):
+        self.__net = cv2.dnn.readNet(prototxtPath, modelPath)
+        if self.__net is None:
+            print("Face Detector 클래스 생성 실패, 프로그램 종료")
+            sys.exit()
+
+        print("Face Detector 클래스 생성 완료")
+    
     def GetDetectionsFromFrame(self, frame):
         blob = cv2.dnn.blobFromImage(frame, 1.0, (300,300), (104.0,177.0,123.0))
-        self.net.setInput(blob)
-        return self.net.forward() #순방향 추론
-
-    def NumberOfDection(detections):
-        return range(0,detections.shape[2])
+        self.__net.setInput(blob)
+        detections = self.__net.forward()#순방향 추론
+        return detections[0,0]
     
-    def GetConfidence(detections, i):
-        return detections[0,0,i,2]
+    @staticmethod
+    def GetConfidence(detection):
+        return detection[2]
+
+    @staticmethod
+    def GetFaceLocation(detection, frame):
+        (height,width) = np.shape(frame)[:2]
+        print(height,width)
+        faceLocation  = detection[3:7] * np.array([width,height,width,height])
+        (left, top, right, bottom) = faceLocation.astype("int")
+        top = max(0,top)
+        bottom = min(height-1,bottom)
+        left = max(0,left)
+        right = min(width-1,right)        
+
+        return frame[top:bottom,left:right]
+
+    @staticmethod    
+    def GetFaceLocationForMD(detection, frame):
+
+        face = FaceDetector.GetFaceLocation(detection,frame)
+        face = cv2.cvtColor(face,cv2.COLOR_BGR2RGB)
+        face = cv2.resize(face, (224, 224))
+
+        return face
