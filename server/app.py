a92ecc7553cd3902c9bca681837d2a072141629a
CastleJo <josw631@gmail.com>
2021-04-16 20:20:43 +0900

    feat: rds 연결

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from .routes import match
 from server.tools.mysql.mysql import SingletonSQL
 
 #TODO: SQL Disconnect 타이밍을 모르겠음
-SingletonSQL.instance(user='root', passwd='1234', host='127.0.0.1', db ='frames')
+SingletonSQL.instance(user='admin', passwd='kpu123456!', host='framesdb.cys6irkoowji.ap-northeast-2.rds.amazonaws.com', db ='frames')
 
 app = Flask(__name__)
 app.register_blueprint(match.bp)
