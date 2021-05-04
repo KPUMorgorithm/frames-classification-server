aca5e4ba1468459471ad21aa2076772411a3dee2
outstandingboy <outstanding1301@gmail.com>
2021-05-04 21:34:41 +0900

    refactor: db 리팩토링

--- server/routes/match.py
+++ server/routes/match.py
@@ -5,7 +5,10 @@ from flask import request, Blueprint
 from server.tools.face_tool import FaceTool
 from server.tools.face_detector import FaceDetector
 from server.tools.mask_detector import MaskDetector
-from server.tools.mysql.mysql import  SingletonSQL
+
+from server.db.db import  DB
+
+from server.store.checklistStore import Checklist, MemberDict
 
 ft = FaceTool()
 fd = FaceDetector('server/tools/face_detector/deploy.prototxt',
@@ -17,7 +20,7 @@ bp = Blueprint('match', __name__, url_prefix='/match')
 @bp.route('/', methods=['POST'], strict_slashes=False)
 def match():
 
-    sql = SingletonSQL.instance()
+    db = DB.instance()
     result = []
     nparr = np.fromstring(request.files['frame'].read(), np.uint8) # 리퀘스트로 버퍼 읽기
     frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # 버퍼 -> Mat
@@ -49,7 +52,8 @@ def match():
                           ))
                           
             # sql.insertStatus(state=state, facilityNum=fNum, memberNum=1, temperature=tp)
-            sql.insertStatus(state=0, facilityNum=3, memberNum=1, temperature=tp)
+            checklist: MemberDict = Checklist.instance()
+            checklist.check(1, ifNotExists=lambda: db.status.insertStatus(state=0, facilityNum=3, memberNum=1, temperature=tp, regdate=datetime.now() + timedelta(seconds=10)))
        
             print(f"tp = {tp}, fNum = {fNum}, state = {state}, name = {name}")
 
