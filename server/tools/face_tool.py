cadc2f2091dfbd1caccf9f7dcdf365313b589fea
CastleJo <josw631@gmail.com>
2021-06-08 16:05:42 +0900

    feat: mask detection in server

--- server/tools/face_tool.py
+++ server/tools/face_tool.py
@@ -10,17 +10,25 @@ class FaceTool():
 
         self.contextFaceList = self.faceList.getListByQueue()
     
-    # @staticmethod
-    # def feature(frame):
-    #     rgb_frame = frame[:, :, ::-1]
+    @staticmethod
+    def feature(frame):
+        rgb_frame = frame[:, :, ::-1]
 
-    #     face_locations = face_recognition.face_locations(rgb_frame, number_of_times_to_upsample=0)
-    #     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+        face_locations = face_recognition.face_locations(rgb_frame, number_of_times_to_upsample=0)
         
-    #     return (face_locations, face_encodings)
+        if face_locations == []:
+            return None
+                
+        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+        
+        return face_encodings
 
     def match(self, landmark):
         name = "Unknown"
+
+        if landmark is None:
+            return name, None
+
         face_distances = face_recognition.face_distance(list(map(lambda x: x[1], self.contextFaceList)), self.__convertListLandmark(landmark))
         best_match_index = np.argmin(face_distances)
         if face_distances[best_match_index] <= 0.5:
--- server/tools/mask_detector.py
+++ server/tools/mask_detector.py
@@ -3,6 +3,7 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 import sys
 import numpy as np
+import cv2
 
 class MaskDetector:
     def __init__(self, modelPath):
@@ -12,10 +13,17 @@ class MaskDetector:
             sys.exit()
         print("Mask Detector 클래스 생성 완료")
         
-    def MaskDetection(self, face):
+    def isMasked(self, face):
+
+        face = cv2.resize(face,(224,224))
+
         face = img_to_array(face)
         face = preprocess_input(face)
         face = np.expand_dims(face, axis = 0)
 
-        #return (mask, withoutMask)
-        return self._model.predict(face)[0]
+        print(self._model.predict(face)[0])
+        (mask, withoutMask) = self._model.predict(face)[0]
+
+        if mask>withoutMask:
+            return True
+        return False
