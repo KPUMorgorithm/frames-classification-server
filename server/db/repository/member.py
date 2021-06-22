872c82a7ca6227eddd779a4b82b63f9b9092a5cf
outstandingboy <outstanding1301@gmail.com>
2021-06-23 00:24:21 +0900

    feat: member repository

---
+++ server/db/repository/member.py
@@ -0,0 +1,54 @@
+from datetime import datetime, timedelta
+import traceback
+
+class MemberRepository:
+    def __init__(self, db):
+        self.__db = db
+
+    def getMembers(self):
+        try:    
+            with self.__db.getConnection() as conn:
+                with conn.cursor() as cursor:
+                    sql = f'SELECT * FROM member'
+                    cursor.execute(sql)
+                    conn.commit()
+                    return cursor.fetchall()
+
+        except Exception as e:
+            traceback.print_exc()
+            
+    def getMemberImages(self, mno):
+        try:    
+            with self.__db.getConnection() as conn:
+                with conn.cursor() as cursor:
+                    sql = f'SELECT * FROM member_image WHERE member_mno={mno}'
+                    cursor.execute(sql)
+                    conn.commit()
+                    return cursor.fetchall()
+
+        except Exception as e:
+            traceback.print_exc()
+
+    def getMembersWithImages(self):
+        _members = self.getMembers()
+
+        members = []
+        for member in _members:
+            print(member)
+            _member_images = self.getMemberImages(mno=member[0])
+            print(_member_images)
+            images = []
+            for member_image in _member_images:
+                images.append({
+                    "id": member_image[0],
+                    "name": member_image[1],
+                    "path": member_image[2],
+                    "uuid": member_image[3]
+                })
+            members.append({
+                "mno": member[0],
+                "name": member[1],
+                "phone": member[2],
+                "images": images
+            })
+        return members
