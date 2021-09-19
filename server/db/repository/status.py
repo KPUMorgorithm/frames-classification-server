a5dd971cb9e71c7a7857c695c72a4b807b0a2864
UkCastle <jo631@naver.com>
2021-09-19 09:30:05 +0000

    feat: db autoincrement fix

--- server/db/repository/status.py
+++ server/db/repository/status.py
@@ -5,13 +5,35 @@ class StatusRepository:
     def __init__(self, db):
         self.__db = db
     
-    def insertStatus(self, state, facilityNum, memberNum, temperature, regdate=datetime.now()):
-            try:    
-                with self.__db.getConnection() as conn:
-                    with conn.cursor() as cursor:
-                        sql = f'INSERT INTO status(state, facility_bno, member_mno, temperature, regdate) VALUES (0, 3, {memberNum}, {temperature}, "{regdate}")'
-                        cursor.execute(sql)
-                    conn.commit()
+    def initAutoIncrement(self):
+        try:    
+            with self.__db.getConnection() as conn:
+                with conn.cursor() as cursor:
+                    sql = "ALTER TABLE status AUTO_INCREMENT=1"
+                    cursor.execute(sql)
+                conn.commit()
+                with conn.cursor() as cursor:
+                    sql = "set @cnt = 0"
+                    cursor.execute(sql)
+                conn.commit()
+                with conn.cursor() as cursor:
+                    sql = 'update status set status.statusnum = @cnt:=@cnt+1'
+                    cursor.execute(sql)
+                conn.commit()
+                
+        except Exception as e:
+            traceback.print_exc()
 
-            except Exception as e:
-                traceback.print_exc()
+    def insertStatus(self, state, facilityNum, memberNum, temperature, regdate=datetime.now()):
+        regdate = regdate + timedelta(hours=9)
+        print(regdate)
+        self.initAutoIncrement()
+        try:    
+            with self.__db.getConnection() as conn:
+                with conn.cursor() as cursor:
+                    sql = f'INSERT INTO status(state, facility_bno, member_mno, temperature, regdate) VALUES ({state}, {facilityNum}, {memberNum}, {temperature}, "{regdate}")'
+                    cursor.execute(sql)
+                conn.commit()
+
+        except Exception as e:
+            traceback.print_exc()
